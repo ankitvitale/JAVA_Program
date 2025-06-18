a9f2dd4a78b7d479e8422cdd4ef9083510835b2f
Ankit <ankitvitale18@gmail.com>
2025-06-18 23:46:53 +0530

    add

--- src/Ankit.java
+++ src/Ankit.java
@@ -1,5 +1,10 @@
 public class Ankit {
     public static void main(String[] args) {
         System.out.println(" i am ankit");
+
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
     }
 }
